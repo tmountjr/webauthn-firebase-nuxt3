410d5a4a29e9c012ddeee0bbb1836d298f00c9d0
Tom Mount <tmountjr@gmail.com>
2023-02-14 16:36:26 -0500

    add step 2 (passkey)

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-
 const userStore = useAnonUserStore()
 
 definePageMeta({
@@ -15,7 +13,15 @@ definePageMeta({
  */
 const step = ref(1)
 
+const currentChallenge = ref('')
+
 const actionStep = computed(() => step.value < 3 ? 'Continue' : 'Back')
+
+const back = () => {
+  if (step.value > 1) {
+    step.value = step.value - 1
+  }
+}
 
 /**
  * Process:
@@ -34,6 +40,7 @@ const nextStep = async () => {
       await userStore.getCredentials()
       if (userStore.hasUserCreds) {
         // valid user, has authentication devices
+        // The getFbUid and getCredentials will populate the store with the necessary values.
         step.value = 2
       } else {
         // valid user, has no authentication devices
@@ -45,6 +52,64 @@ const nextStep = async () => {
     }
   } else if (step.value === 2) {
     // Handle click event for logging in with passkey
+
+    // PART 1: Authentication Options
+    const { startAuthentication } = await import('@simplewebauthn/browser')
+    let optsResponse: Response
+    try {
+      optsResponse = await fetch('/auth/generate-authentication-options', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          fbUid: userStore.fbUid
+        }),
+      })
+    } catch (e) {
+      console.error('error getting auth options', e)
+      return
+    }
+    const opts = await optsResponse.json()
+    currentChallenge.value = opts.challenge
+    // END PART 1
+
+    // PART 2: Get authentication data from provider (browser)
+    let authCredResponse
+    try {
+      authCredResponse = await startAuthentication(opts)
+    } catch (e) {
+      console.error('error getting authenticator response', e)
+      return
+    }
+    // END PART 2
+
+    // PART 3: Verify provider's data
+    try {
+      const verificationResp = await fetch('/auth/verify-authentication', {
+        method: 'POST',
+        body: JSON.stringify({
+          currentChallenge: currentChallenge.value,
+          body: authCredResponse,
+          fbUid: userStore.fbUid,
+          withToken: true,
+        }),
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
+
+      const { verified, token } = await verificationResp.json()
+      if (verified) {
+        console.log('TODO: login with token', token)
+      } else {
+        console.error('authenticator not verified')
+      }
+    } catch (e) {
+      console.error('error while authenticating', e)
+    }
+    // END PART 3
+
   } else if (step.value === 3) {
     // Handle click event for logging in with password
     // Account may not exist at this point.
@@ -64,18 +129,25 @@ const nextStep = async () => {
             <v-card-text>
               <v-window v-model="step">
                 <v-window-item :value="1">
-                  <v-text-field label="Email" v-model="userStore.email" variant="solo" />
+                  <v-text-field label="Email" v-model="userStore.email" variant="underlined" />
+                  <div class="d-flex">
+                    <v-btn @click="nextStep" class="flex-grow-1">{{ actionStep }}</v-btn>
+                  </div>
                 </v-window-item>
 
                 <v-window-item :value="2">
-                  <v-btn block @click="nextStep">Log In With Passkey</v-btn>
+                  <v-text-field :value="userStore.email" variant="underlined" disabled></v-text-field>
+                  <div class="buttons d-flex">
+                    <v-btn @click="back" class="flex-grow-1">Go Back</v-btn>
+                    <v-btn @click="nextStep" class="flex-grow-1">Log In With Passkey</v-btn>
+                  </div>
                 </v-window-item>
 
                 <v-window-item :value="3">
-                  <v-text-field type="password" label="Password" variant="solo" />
+                  <v-text-field type="password" label="Password" variant="underlined" />
                 </v-window-item>
               </v-window>
-              <v-btn block @click="nextStep">{{ actionStep }}</v-btn>
+              
             </v-card-text>
           </v-card>
         </v-col>
@@ -83,3 +155,9 @@ const nextStep = async () => {
     </v-container>
   </div>
 </template>
+
+<style scoped>
+div.buttons button {
+  flex-basis: 50%;
+}
+</style>
