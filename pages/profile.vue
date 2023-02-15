5b032a7d6c4b4a4390011126fef2fb205d324a64
Tom Mount <tmountjr@gmail.com>
2023-02-15 15:23:42 -0500

    get profile page working
    still want to clean up the store and avoid the "attachListeners" nonsense

---
+++ pages/profile.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+const authUserStore = useAuthUserStore()
+onMounted(async () => {
+  await authUserStore.attachListeners()
+})
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="4">
+        <h1>Public Profile</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="3">
+        <p><strong>Name:</strong> {{ authUserStore.profile.name }}</p>
+      </v-col>
+      <v-col cols="12" md="3">
+        <p><strong>Favorite Color:</strong></p>
+        <v-color-picker v-model="authUserStore.profile.favoriteColor" :modes="['rgb']" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-btn block @click="authUserStore.saveToProfile">Save Profile Information</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- store/user.authenticated.ts
+++ store/user.authenticated.ts
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore, acceptHMRUpdate } from 'pinia'
+import { ref as dbRef, onValue, Unsubscribe, get, set } from 'firebase/database'
 import {
   signInWithCustomToken,
   User,
@@ -9,55 +10,87 @@ import {
 } from 'firebase/auth'
 
 export const useAuthUserStore = defineStore('user.authenticated', () => {
-  const { $auth } = useNuxtApp()
-  const stateChangeListener = (authUser: User | null) => {
-    if (authUser) {
-      user.value = authUser
-    } else {
-      user.value = undefined
-
-      // Since we're logged out, navigate back to the user-requested page
-      navigateTo(logoutRedirectDest.value)
-    }
-  }
+  const { $auth, $database } = useNuxtApp()
+  let unsubscribeProfile: Unsubscribe | undefined
 
   // Refs
   /** The Firebase Auth user */
   const user = ref<User>()
   const logoutRedirectDest = ref('/login')
+  const profile = ref()
 
   // Computed
   const isAuthenticated = computed<boolean>(() => user.value !== undefined)
 
   // Methods
   const tokenAuth = async (token: string) => {
-    $auth.onAuthStateChanged(stateChangeListener)
     await signInWithCustomToken($auth, token)
   }
 
   const createUser = async (email: string, password: string) => {
-    $auth.onAuthStateChanged(stateChangeListener)
     await createUserWithEmailAndPassword($auth, email, password)
   }
 
   const loginUser = async (email: string, password: string) => {
-    $auth.onAuthStateChanged(stateChangeListener)
     await signInWithEmailAndPassword($auth, email, password)
   }
 
   const logout = async (redirectTo: string = logoutRedirectDest.value) => {
     logoutRedirectDest.value = redirectTo
-    $auth.onAuthStateChanged(stateChangeListener)
     await signOut($auth)
+  }
+
+  const saveToProfile = async () => {
+    if (user.value && profile.value) {
+      const profileRef = dbRef($database, `/users/${user.value.uid}/profile`)
+      await set(profileRef, profile.value)
+    }
+  }
+
+  const attachListeners = async () => {
+    $auth.onAuthStateChanged(async (authUser: User | null) => {
+      console.debug('Auth State Changed')
+      if (authUser) {
+        user.value = authUser
+
+        // Since the state has changed, we need to refresh the profile data.
+        const profileRef = dbRef($database, `/users/${authUser.uid}/profile`)
+        unsubscribeProfile = onValue(profileRef, (snapshot) => {
+          console.debug('Profile Information Changed')
+          const data = snapshot.val()
+          profile.value = data
+        })
+      } else {
+        user.value = undefined
+        profile.value = undefined
+        if (unsubscribeProfile) {
+          unsubscribeProfile()
+        }
+
+        // Since we're logged out, navigate back to the user-requested page
+        navigateTo(logoutRedirectDest.value)
+      }
+    })
+
+    // If we have a user available, get the profile data
+    if (user.value) {
+      const profileSnapshot = await get(dbRef($database, `/users/${user.value.uid}/profile`))
+      if (profileSnapshot.exists()) {
+        profile.value = profileSnapshot.val()
+      }
+    }
   }
 
   return {
     user,
+    profile,
     isAuthenticated,
     tokenAuth,
     createUser,
     loginUser,
     logout,
+    saveToProfile,
+    attachListeners,
   }
 }, { persist: true })
 
